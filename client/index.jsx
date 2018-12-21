bc1cab8643325167876141ad1f5ac51fc8d9eb58
shakabandai <jus10daniel@gmail.com>
2018-12-21 10:27:17 -0600

    shifted client file structure

---
+++ client/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Sources from './components/sources.jsx';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+    <div>
+    <div>Hello There</div>
+    <div><Sources /></div>
+    </div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ client/src/components/sources.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Sources extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+    <div>We will be sources!</div>
+    }
+}
+export default Sources;
