f6a3434f89445870849a44c4c30bee90aa70f6a1
shakabandai <jus10daniel@gmail.com>
2018-12-21 10:43:03 -0600

    passing props for track/playlist info

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Sources from './src/components/sources.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            tracks : [],
+            playlists: []
+        }
     }
+
+    
     render(){
     return(<div>
     <div>Hello There</div>
-    <div><Sources /></div>
+    <div><Sources tracks={this.state.tracks} playlists={this.state.playlists}/></div>
     </div>)
     }
 }
