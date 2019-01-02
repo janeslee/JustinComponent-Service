3ea8b9737d11559815fb1bac56112cfbb510d3db
shakabandai <jus10daniel@gmail.com>
2019-01-02 01:12:56 -0600

    updated styling

--- client/src/components/inAlbums.jsx
+++ client/src/components/inAlbums.jsx
@@ -7,13 +7,28 @@ class InAlbums extends React.Component {
 
 
   render() {
+    let background = this.props.currentTrack.albumArt;
     return (
       <div><span>Albums ||</span><span> View All</span>
-        <div>
-          <div>{this.props.currentTrack.artist}</div>
-          <div><span>{this.props.currentTrack.albumArt} |</span><span>| {this.props.currentTrack.track}</span></div>
-          <div><span>{this.props.currentTrack.plays} ||</span><span> <button>{this.props.currentTrack.likes} </button>||</span><span> <button>{this.props.currentTrack.shares}</button> ||</span><span>{this.props.currentTrack.comments}</span></div>
-        </div>
+        <ul style={{listStyle: 'none', margin: 0, padding: 0}}>
+
+          <li>
+            <div style={{display: 'flex', flexDirection: 'row'}} >
+              <span style={{flex: 1}}>
+                <div style={{ backgroundImage: `url(${background})`, width: 50, height: 50, opacity: 1 }} />
+              </span>
+
+              <div style={{flex: 9}}>
+                <div>{this.props.currentTrack.artist}</div>
+                <div><span>{this.props.currentTrack.track}</span></div>
+                <div><span>{this.props.currentTrack.plays} ||</span><span> <button>{this.props.currentTrack.likes} </button>||</span><span> <button>{this.props.currentTrack.shares}</button> ||</span><span>{this.props.currentTrack.comments}</span></div>
+              </div>
+
+            </div>
+
+          </li>
+
+        </ul>
       </div>
     )
 
--- client/src/components/inPlaylists.jsx
+++ client/src/components/inPlaylists.jsx
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 
 
-class InPlaylists extends React.Component{
-  constructor(props){
+class InPlaylists extends React.Component {
+  constructor(props) {
     super(props);
   }
 
-  render(){
+  render() {
     return (
       <div><span>Playlists ||</span><span> View All</span>
-      {this.props.relatedTracks.map(track => {
-        return (
-          <div>
-          <div>{track.artist}</div>
-          <div><span>{track.albumArt} |</span><span>| {track.track}</span></div>
-          <div><span>{track.plays} ||</span><span> <button>{track.likes} </button>||</span><span> <button>{track.shares}</button> ||</span><span>{track.comments}</span></div>
-          </div>
-        )
-      })
-      }
+        <ul style={{listStyle: 'none', margin: 0, padding: 0}}>
+          {this.props.relatedTracks.map(track => {
+            let background = track.albumArt;
+            return (
+              <li>
+                <div style={{display: 'flex', flexDirection: 'row'}}>
+                  <span style={{ flex: 1 }}>
+                    <div style={{ backgroundImage: `url(${background})`, width: 50, height: 50, opacity: 1 }} />
+                  </span>
+
+                  <div style={{ flex: 9 }}>
+                    <div>{track.artist}</div>
+                    <div>{track.track}</div>
+                    <div><span>{track.plays} ||</span><span> <button>{track.likes} </button>||</span><span> <button>{track.shares}</button> ||</span><span>{track.comments}</span></div>
+                  </div>
+                </div>
+              </li>
+            )
+          })
+          }
+        </ul>
       </div>
     )
-    
+
   }
 
 }
